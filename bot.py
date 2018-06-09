76e1a71200526991df8bb0a502ad57ac59fb1c98
Zachary Stence <zachstence@gmail.com>
2018-06-09 18:18:39 -0400

    Add Twitch membership/tag/commands support
    - Send additional requests to Twitch to get membership/tag/command information for more sophisticated bot actions
    - Create process_response.py as a template to process different responses from server
    - Rename commands.py, user_commands.py

--- bot.py
+++ bot.py
@@ -4,7 +4,8 @@ import time
 from inspect import signature
 
 import cfg
-import commands
+import user_commands
+import process_response
 import helix
 
 def chat(sock, msg):
@@ -64,16 +65,25 @@ def run_command(user, message):
     return func(*args)
 
 #############################################################################################################
+# subscription command         <tags>:tmi.twitch.tv USERNOTICE #<channel> :<message>
+# private message command      <tags>:<user>!<user>@<user>.tmi.twitch.tv PRIVMSG #<channel> :<message>
+# in general                   tmi.twitch.tv <COMMAND> #<channel>
+
+
 
 try:
 
   s = socket.socket()
   s.connect((cfg.HOST, cfg.PORT))
+  s.send("CAP REQ :twitch.tv/membership\r\n".encode('utf-8'))
+  s.send("CAP REQ :twitch.tv/tags\r\n".encode('utf-8'))
+  s.send("CAP REQ :twitch.tv/commands\r\n".encode('utf-8'))
+
   s.send("PASS {}\r\n".format(cfg.PASS).encode("utf-8"))
   s.send("NICK {}\r\n".format(cfg.NICK).encode("utf-8"))
   s.send("JOIN #{}\r\n".format(cfg.CHAN).encode("utf-8"))
   
-  chat(s, "Bot connected!")
+  # chat(s, "Bot connected!")
   
   # compile server response format into RegEx object
   CHAT_MSG = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
@@ -82,37 +92,66 @@ try:
   while True:
     # get server response
     response = s.recv(1024).decode("utf-8")
+    
   
     # else server pings us, pong back to let it know we're still connected
     if response == "PING :tmi.twitch.tv\r\n":
       s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
   
-    # else reponse is a chat message
     else:
-      # get username from response
-      username = re.search(r"\w+", response).group(0)
-  
-      # subtract unnecessary information from response, leaving just chat message
-      message = CHAT_MSG.sub("", response)
-  
-      # print chat message to console
-      print(username + ": " + message)
-  
-      for pattern in cfg.BANNED:
-        if re.match(pattern, message):
-          ban(s, username)
-          break
-  
-      for pattern, duration in cfg.TIMEOUT.items():
-        if re.match(pattern, message):
-          timeout(s, username, duration)
-          break
-  
-      if message[0] == "!":
-        chat(s, run_command(username, message))
-  
-    time.sleep(1 / cfg.RATE)
-  
+      print(response)
+      m = re.search(r"tmi\.twitch\.tv (\w+) #", response)
+      if m != None:
+        reponse_type = m.group(1)
+
+        response_types = {
+          'JOIN'            : process_response.JOIN,
+          'PART'            : process_response.PART,
+
+          'CLEARCHAT'       : process_response.CLEARCHAT,
+          'GLOBALUSERSTATE' : process_response.GLOBALUSERSTATE,
+          'PRIVMSG'         : process_response.PRIVMSG,
+          'ROOMSTATE'       : process_response.ROOMSTATE,
+          'USERNOTICE'      : process_response.USERNOTICE,
+          'USERSTATE'       : process_response.USERSTATE,
+
+          'HOSTTARGET'      : process_response.HOSTTARGET,
+          'NOTICE'          : process_response.NOTICE,
+          'RECONNECT'       : process_response.RECONNECT
+
+        }
+
+        # call appropriate function to process response
+        response_types[reponse_type]()
+
+
+
+#     # else reponse is a chat message
+#     else:
+#       # get username from response
+#       username = re.search(r"\w+", response).group(0)
+#   
+#       # subtract unnecessary information from response, leaving just chat message
+#       message = CHAT_MSG.sub("", response)
+#   
+#       # print chat message to console
+#       print(username + ": " + message)
+#   
+#       for pattern in cfg.BANNED:
+#         if re.match(pattern, message):
+#           ban(s, username)
+#           break
+#   
+#       for pattern, duration in cfg.TIMEOUT.items():
+#         if re.match(pattern, message):
+#           timeout(s, username, duration)
+#           break
+#   
+#       if message[0] == "!":
+#         chat(s, run_command(username, message))
+#   
+#     time.sleep(1 / cfg.RATE)
+#   
 except KeyboardInterrupt as e:
   print("\nBot disconnected!")
-  chat(s, "Bot disconnected!")
+  # chat(s, "Bot disconnected!")
---
+++ process_response.py
@@ -0,0 +1,37 @@
+#### Membership
+def JOIN():
+  print('JOIN')
+
+def PART():
+  print('PART')
+
+
+#### Tags
+def CLEARCHAT():
+  print('CLEARCHAT')
+
+def GLOBALUSERSTATE():
+  print('GLOBALUSERSTATE')
+
+def PRIVMSG():
+  print('PRIVMSG')
+
+def ROOMSTATE():
+  print('ROOMSTATE')
+
+def USERNOTICE():
+  print('USERNOTICE')
+
+def USERSTATE():
+  print('USERSTATE')
+
+
+#### Commands
+def HOSTTARGET():
+  print('HOSTTARGET')
+
+def NOTICE():
+  print('NOTICE')
+
+def RECONNECT():
+  print('RECONNECT')
